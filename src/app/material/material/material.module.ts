f26ead476ba86d86acb30289f59200ac262f7428
wayne9499 <dheerajsharma9499@gmail.com>
2020-08-17 20:38:25 +0530

    some refactoring and minor improvements

--- src/app/material/material/material.module.ts
+++ src/app/material/material/material.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
 
 const material = [
   MatFormFieldModule,
   MatButtonModule,
   MatInputModule,
   MatMenuModule,
-  MatIconModule
+  MatIconModule,
+  MatDialogModule
 ];
 
 
