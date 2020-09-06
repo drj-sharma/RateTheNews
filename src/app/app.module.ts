55ab37f33006c00068a99766a004b42570e58712
Paramveer12 <paramveer.2023@gmail.com>
2020-09-06 20:09:37 +0530

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MaterialModule } from './material/material/material.module';
-import {MatTabsModule} from '@angular/material/tabs';
 import { AnchorComponent } from './anchor/anchor.component';
 
 
@@ -37,7 +36,6 @@ import { AnchorComponent } from './anchor/anchor.component';
     AngularFirestoreModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
