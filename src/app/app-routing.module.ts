4871829a2f042ee7311320b9285f74ee65beba20
wayne9499 <dheerajsharma9499@gmail.com>
2020-09-08 17:13:03 +0530

    editor integration in article page #15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AnchorComponent } from './anchor/anchor.component';
+import { ArticleComponent } from './article/article/article.component';
 // import { AuthComponent } from './auth/auth/auth.component';
 import {MainComponent} from './main/main.component';
 
@@ -8,7 +9,8 @@ import {MainComponent} from './main/main.component';
 const routes: Routes = [
   // {path: '', component: AuthComponent},
   { path: '', component: MainComponent },
-  { path: 'anchor', component: AnchorComponent }
+  { path: 'anchor', component: AnchorComponent },
+  { path: 'article', component: ArticleComponent}
 ];
 
 @NgModule({
