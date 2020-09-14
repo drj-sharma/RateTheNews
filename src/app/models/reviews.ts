35b5abd97ea3d7342271bc57c342631615592385
Paramveer12 <paramveer.2023@gmail.com>
2020-09-14 16:52:19 +0530

    added review saving

---
+++ src/app/models/reviews.ts
@@ -0,0 +1,11 @@
+export interface reviews {
+    id?: string;
+    title?: string;
+    content?: string;
+    user?: string;
+    rating?: number;
+    helpfull?: number;
+    nothelpfull?: number;
+    time?: Date;
+    show?: string;
+}
--- src/app/review-comp/review-comp.component.ts
+++ src/app/review-comp/review-comp.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import {WriteReviewComponent} from '../write-review/write-review.component'
+import { LoginComponent } from 'src/app/login/login.component';
+import * as firebase from "firebase/app";
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-review-comp',
@@ -8,17 +11,35 @@ import {WriteReviewComponent} from '../write-review/write-review.component'
   styleUrls: ['./review-comp.component.scss']
 })
 export class ReviewCompComponent implements OnInit {
+  a =false;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog,private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
-  openDialog(): void {
-    const dialogRef = this.dialog.open(WriteReviewComponent);
+  
+  openDialog(){
+    var user = firebase.auth().currentUser;
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
+    if (user!= null) {
+        const dialogRef = this.dialog.open(WriteReviewComponent);
 
+        dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        });
+      } else {
+        const dialogRef2 = this.dialog.open(LoginComponent);
+
+        dialogRef2.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        });
+        this.openSnackBar('Login to write review', 'OKAY');
+
+      }
+}
+openSnackBar(msg: string, action: string) {
+  this.snackBar.open(msg, action, {
+    duration: 2000,
+  });
+}
 }
